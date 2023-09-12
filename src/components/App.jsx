d2f93289e97a4db777208ff6e70c31d4723bf360
vldan <vlad.danilov.020212@gmail.com>
2023-09-12 16:14:36 +0300

    feat: процесс разработки темной темы

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,10 +6,16 @@ import Stack from "./Stack";
 import Projects from "./Projects";
 import Footer from "./Footer";
 import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
   const [isReactTabActive, setIsReactTabActive] = useState(false);
   const [isLayoutTabActive, setIsLayoutTabActive] = useState(true);
+  const [isThemeDark, setIsThemeDark] = useState(false);
+
+  const hadleSwitchTheme = () => {
+    setIsThemeDark(true);
+  };
 
   const handleClickLayoutTab = () => {
     setIsReactTabActive(false);
@@ -21,9 +27,17 @@ function App() {
     setIsLayoutTabActive(false);
   };
 
+  useEffect(() => {
+    if (isThemeDark) {
+      document.documentElement.classList.add("root_theme_dark");
+    } else {
+      document.documentElement.classList.remove("root_theme_dark");
+    }
+  }, [isThemeDark]);
+
   return (
-    <div className="body">
-      <Header />
+    <>
+      <Header onSwitchTheme={hadleSwitchTheme} />
       <main className="main">
         <Welcome />
         <AboutMe />
@@ -37,7 +51,7 @@ function App() {
         />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,36 @@
 import russian from "../images/russian.svg";
 import dropdownArrow from "../images/dropdown-arrow.svg";
-function Header() {
+function Header({ onSwitchTheme }) {
   return (
-      <header className="header">
-        <div className="header__container">
-          <div className="toggle">
-            <input type="checkbox" id="toggle_checkbox" />
-            <label htmlFor="toggle_checkbox">
-              <div id="star">
-                <div className="star" id="star-1">
-                  ★
-                </div>
-                <div className="star" id="star-2">
-                  ★
-                </div>
+    <header className="header">
+      <div className="header__container">
+        <div className="toggle">
+          <input
+            type="checkbox"
+            id="toggle_checkbox"
+            onChange={onSwitchTheme}
+          />
+          <label htmlFor="toggle_checkbox">
+            <div id="star">
+              <div className="star" id="star-1">
+                ★
               </div>
-              <div id="moon"></div>
-            </label>
-          </div>
-          <div className="dropdown">
-            <button className="dropdown__btn">
-              <img src={russian} alt="#" className="dropdown__img" />
-              <p className="dropdown__text">RU</p>
-              <img src={dropdownArrow} alt="#" className="dropdown__arrow" />
-            </button>
-          </div>
+              <div className="star" id="star-2">
+                ★
+              </div>
+            </div>
+            <div id="moon"></div>
+          </label>
         </div>
-      </header>
+        <div className="dropdown">
+          <button className="dropdown__btn">
+            <img src={russian} alt="#" className="dropdown__img" />
+            <p className="dropdown__text">RU</p>
+            <img src={dropdownArrow} alt="#" className="dropdown__arrow" />
+          </button>
+        </div>
+      </div>
+    </header>
   );
 }
 
