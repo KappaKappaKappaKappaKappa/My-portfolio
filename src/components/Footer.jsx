5c84912e79687a9c4ff071f6fb2a7d12af9ccd8f
vldan <vlad.danilov.020212@gmail.com>
2023-09-12 14:11:46 +0300

    feat: Create section Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import footerGitImg from "../images/footer-github-img.svg";
+import footerTelegramImg from "../images/footer-telegram-img.svg";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <ul className="footer-list">
+          <li className="footer-list__item">
+            <a
+              href="https://github.com/KappaKappaKappaKappaKappa"
+              className="footer-list__item-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="footer-list__item-img"
+                src={footerGitImg}
+                alt="Значок Github"
+              />
+              <span className="footer-list__item-text">GitHub</span>
+            </a>
+          </li>
+
+          <li className="footer-list__item">
+            <a
+              href="https://github.com/KappaKappaKappaKappaKappa"
+              className="footer-list__item-link"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                className="footer-list__item-img"
+                src={footerTelegramImg}
+                alt="Значок Telegram"
+              />
+              <span className="footer-list__item-text">Telegram</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
